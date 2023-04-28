d259c905ed2b5b0c6ed6450f43a41e2be968cebc
jriscos <riscos1970@gmail.com>
2023-04-28 11:48:55 +0200

    first commit

---
+++ src/main/java/com/mycompany/tests/pau/javi/jesus/TestsPauJaviJesus.java
@@ -0,0 +1,8 @@
+package com.mycompany.tests.pau.javi.jesus;
+
+public class TestsPauJaviJesus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/tests/pau/javi/jesus/TestsClassTest.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.tests.pau.javi.jesus;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Jaavii
+ */
+public class TestsClassTest {
+    
+    public TestsClassTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
