81da08a8cc9f2628ddd8ed6f59e7c92bde78ce1f
jeam226 <jeam226@vidalibarraquer.net>
2023-05-03 10:09:30 +0200

    Aplicació acabada

--- src/main/java/com/mycompany/tests/pau/javi/jesus/TestsPauJaviJesus.java
+++ src/main/java/com/mycompany/tests/pau/javi/jesus/TestsPauJaviJesus.java
@@ -10,20 +10,30 @@ public class TestsPauJaviJesus {
         Scanner sc = new Scanner(System.in);
 
         int eleccioUsusari = programa.imprimirMenu(sc);
+        int num1, num2, resultat;
 
         while (eleccioUsusari != 0) {
-            switch (eleccioUsusari){
-                case 1:
-                
-                    break;
-    
-                case 2:
-    
-                    break;
-    
-                case 3:
-                    
-                    break;
+            switch (eleccioUsusari) {
+                case 1 -> {
+                    num1 = programa.demanar_nombre(sc);
+                    num2 = programa.demanar_nombre(sc);
+                    resultat = programa.suma(num1, num2);
+                    System.out.println("\n\t[*] El resultat és " + resultat);
+                }
+                case 2 -> {
+                    num1 = programa.demanar_nombre(sc);
+                    num2 = programa.demanar_nombre(sc);
+                    resultat = programa.resta(num1, num2);
+                    System.out.println("\n\t[*] El resultat és " + resultat);
+                }
+                case 3 -> {
+                    num1 = programa.demanar_nombre(sc);
+                    if (programa.positiu(num1)) {
+                        System.out.println("\n\t[*] El nombre és positiu");
+                    } else {
+                        System.out.println("\n\t[*] El nombre és negatiu");
+                    }
+                }
             }
             System.out.println();
             eleccioUsusari = programa.imprimirMenu(sc);
@@ -31,22 +41,22 @@ public class TestsPauJaviJesus {
     }
 
     public int imprimirMenu(Scanner sc) {
-        
+
         int x = 0;
 
-        System.out.print("[*] MENÚ CALCULADORA\n" 
-        + "\n\t0 -> Sortir del programa"
-        + "\n\t1 -> Calcular suma de dos nombres" 
-        + "\n\t2 -> Calcular resta de dos nombres" 
-        + "\n\t3 -> Calcular si un nombre es positiu o negatiu" 
-        + "\n\n[+] Tria una opció: ");
-        
+        System.out.print("[*] MENÚ CALCULADORA\n"
+                + "\n\t0 -> Sortir del programa"
+                + "\n\t1 -> Calcular suma de dos nombres"
+                + "\n\t2 -> Calcular resta de dos nombres"
+                + "\n\t3 -> Calcular si un nombre es positiu o negatiu"
+                + "\n\n[+] Tria una opció: ");
+
         try {
             x = sc.nextInt();
         } catch (Exception e) {
             System.out.println("\n[!] Error en l'introducció de les dades!");
         }
-        
+
         return x;
     }
 
@@ -58,15 +68,30 @@ public class TestsPauJaviJesus {
 
     public int resta(int Nombre1, int Nombre2) {
         int x;
-        x = Nombre1 + Nombre2;
+        int aux;
+        if (Nombre2 > Nombre1) {
+            aux = Nombre1;
+            Nombre1 = Nombre2;
+            Nombre2 = aux;
+        }
+        x = Nombre1 - Nombre2;
         return x;
     }
 
     public boolean positiu(int Nombre) {
-        if (Nombre > 0) {
-            return true;            
-        } else {
-            return false;
+        return Nombre > 0;
+    }
+
+    public int demanar_nombre(Scanner sc) {
+        int x;
+        System.out.print("\n\t[+] Introdueix un nombre: ");
+        try {
+            x = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error");
+            x = 0;
         }
+        return x;
+
     }
 }
