98980073e99013cc3488e01dee6c3fccb74d7737
viZkaHD <pauvizcainoluna@gmail.com>
2023-05-04 12:08:28 +0200

    1

--- src/test/java/com/mycompany/tests/pau/javi/jesus/TestsClassTest.java
+++ src/test/java/com/mycompany/tests/pau/javi/jesus/TestsClassTest.java
@@ -3,43 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package com.mycompany.tests.pau.javi.jesus;
+import org.junit.*;
+import org.junit.jupiter.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-/**
- *
- * @author Jaavii
- */
 public class TestsClassTest {
-    
-    public TestsClassTest() {
-    }
 
-    @org.junit.jupiter.api.BeforeAll
-    public static void setUpClass() throws Exception {
-    }
+    /**
+     *
+     * @author PauVizcaino
+     */
 
-    @org.junit.jupiter.api.AfterAll
-    public static void tearDownClass() throws Exception {
-    }
 
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
 
-    @org.junit.jupiter.api.AfterEach
-    public void tearDown() throws Exception {
-    }
 
-    @org.junit.jupiter.api.Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+       /**
+     *
+     * @author JavierRiscos
+     */
+
+
+
+
+     
+       /**
+     *
+     * @author JesusAmeller
+     */
 }
